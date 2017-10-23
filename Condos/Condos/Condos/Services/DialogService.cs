826e89390c2cec77a02f187b3d62e5b91d6b9594
gfva1982 <gfva1982@outlook.com>
2017-10-22 20:33:07 -0600

    Se agrega funcionalidad en el botón de login.

---
+++ Condos/Condos/Condos/Services/DialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Condos.Services
+{
+    public class DialogService
+    {
+        public async Task ShowMessage(string title, string message)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, "Accept");
+        }
+    }
+}
--- Condos/Condos/Condos/ViewModels/LoginViewModel.cs
+++ Condos/Condos/Condos/ViewModels/LoginViewModel.cs
@@ -1,11 +1,167 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Condos.Services;
+
 namespace Condos.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+        #region Properties
 
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(this,
+                                            new PropertyChangedEventArgs(nameof(IsRunning)));
+
+                }
+            }
+        }
+
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(this,
+                                            new PropertyChangedEventArgs(nameof(IsEnabled)));
+
+                }
+            }
+        }
+
+        public bool IsToggled
+        {
+            get
+            {
+                return _isToggled;
+            }
+            set
+            {
+                if (_isToggled != value)
+                {
+                    _isToggled = value;
+                    PropertyChanged?.Invoke(this,
+                                            new PropertyChangedEventArgs(nameof(IsToggled)));
+
+                }
+            }
+        }
+
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    PropertyChanged?.Invoke(this,
+                                            new PropertyChangedEventArgs(nameof(Password)));
+
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    PropertyChanged?.Invoke(this,
+                                            new PropertyChangedEventArgs(nameof(Email)));
+
+                }
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region Attributes
+        string _email;
+
+        string _password;
+
+        bool _isToggled;
+        bool _isEnabled;
+        bool _isRunning;
+
+        #endregion
+
+        #region Services
+        DialogService dialogService;
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await dialogService.ShowMessage("Error", "Debe de ingresar un email valido");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await dialogService.ShowMessage("Error", "Debe de ingresar un password");
+                return;
+            }
+
+
+        }
+
+
+        #endregion
+
+        #region Contructors
         public LoginViewModel()
         {
+            IsEnabled = true;
+            IsToggled = true;
+            dialogService = new DialogService();
         }
+        #endregion
+
+        #region Eventos
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
     }
 }
